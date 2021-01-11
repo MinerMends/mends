4c67dbe0e4e484d45024d619f87772b1e02b0cfc
MinerMends <73631703+MinerMends@users.noreply.github.com>
2021-01-11 11:40:56 -0800

    Update media-only.py

--- media-only/media-only.py
+++ media-only/media-only.py
@@ -30,7 +30,7 @@ class Mediaonly(commands.Cog):
     async def on_message(self, message):
         if self.config.get('status', True) and message.channel.id in self.config.get('channel_ids', []):
             if message.content.contains("https://"):
-              #ignore
+              print('f')
             else:
               
               if message.author.bot:
