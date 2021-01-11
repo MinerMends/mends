7e9e416ab1e2371a9602e8114eafbf6226f85ff7
MinerMends <73631703+MinerMends@users.noreply.github.com>
2021-01-11 11:13:38 -0800

    Update media-only.py

--- media-only/media-only.py
+++ media-only/media-only.py
@@ -29,11 +29,11 @@ class Mediaonly(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if self.config.get('status', True) and message.channel.id in self.config.get('channel_ids', []):
+            if message.content == "eggs"
+              await message.channel.send("working")
             if message.author.bot:
                 await asyncio.sleep(5)
                 await self.delete(message, warning=None)
-                if message.content.startswith('https://'):
-                  return
 
             elif len(message.attachments):
                 if len(message.attachments) > 1: #message.attachments[0].filename.endswith('.gif')
